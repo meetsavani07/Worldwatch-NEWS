cf53bb01a7958b7ba0b55aab4ce598df5b3c26b1
Meet Savani <146066645+meetsavani07@users.noreply.github.com>
2025-04-13 09:44:08 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: '2rem',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
